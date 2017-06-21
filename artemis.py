1dad57c7ae616a4fa16c45e6dfa0bcd716595331
Rhiannon Michelmore <rhiannon.michelmore@gmail.com>
2017-06-21 19:47:03 +0100

    basic karma works, no timeout, unicode or single char exclusion though

--- artemis.py
+++ artemis.py
@@ -83,6 +83,47 @@ async def on_message(message):
 				print(str(ts))
 				cursor.execute(insert_statement, (item.lower(),str(ts),str(ts)))
 				conn.commit()
+			else:
+				iden, score, pluses, minuses, neutrals = rows[0][0], rows[0][4] + 1, rows[0][5] + 1, rows[0][6], rows[0][7]
+				altered = str(datetime.datetime.now(datetime.timezone.utc))
+				update_statement = "UPDATE karma SET (altered,score,pluses,minuses,neutrals) = (%s,%s,%s,%s,%s) WHERE id = (%s);"
+				cursor.execute(update_statement,(altered,score,pluses,minuses,neutrals,iden))
+				conn.commit()
+
+		for item in neg_items:
+			prep_statement = "SELECT * FROM karma WHERE name = (%s);"
+			cursor.execute(prep_statement, [item.lower()])
+			rows = cursor.fetchall()
+			if rows == []:
+				insert_statement = "INSERT INTO karma(name,added,altered,score,pluses,minuses,neutrals) VALUES (%s,%s,%s,-1,0,1,0);"
+				ts = datetime.datetime.now(datetime.timezone.utc)
+				print(str(ts))
+				cursor.execute(insert_statement, (item.lower(),str(ts),str(ts)))
+				conn.commit()
+			else:
+				iden, score, pluses, minuses, neutrals = rows[0][0], rows[0][4] - 1, rows[0][5], rows[0][6] + 1, rows[0][7]
+				altered = str(datetime.datetime.now(datetime.timezone.utc))
+				update_statement = "UPDATE karma SET (altered,score,pluses,minuses,neutrals) = (%s,%s,%s,%s,%s) WHERE id = (%s);"
+				cursor.execute(update_statement,(altered,score,pluses,minuses,neutrals,iden))
+				conn.commit()
+
+		for item in neut_items:
+			prep_statement = "SELECT * FROM karma WHERE name = (%s);"
+			cursor.execute(prep_statement, [item.lower()])
+			rows = cursor.fetchall()
+			if rows == []:
+				insert_statement = "INSERT INTO karma(name,added,altered,score,pluses,minuses,neutrals) VALUES (%s,%s,%s,0,0,0,1);"
+				ts = datetime.datetime.now(datetime.timezone.utc)
+				print(str(ts))
+				cursor.execute(insert_statement, (item.lower(),str(ts),str(ts)))
+				conn.commit()
+			else:
+				iden, score, pluses, minuses, neutrals = rows[0][0], rows[0][4], rows[0][5], rows[0][6], rows[0][7] + 1
+				altered = str(datetime.datetime.now(datetime.timezone.utc))
+				update_statement = "UPDATE karma SET (altered,score,pluses,minuses,neutrals) = (%s,%s,%s,%s,%s) WHERE id = (%s);"
+				cursor.execute(update_statement,(altered,score,pluses,minuses,neutrals,iden))
+				conn.commit()
+
 
 	bot.process_commands(message)
 
